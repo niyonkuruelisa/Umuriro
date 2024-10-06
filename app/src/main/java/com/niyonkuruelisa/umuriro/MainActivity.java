8eca0f7c466e4e53a5fa331ffec1f6b685201dab
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2024-10-06 09:51:41 +0200

    removed button and left a switch

--- app/src/main/java/com/niyonkuruelisa/umuriro/MainActivity.java
+++ app/src/main/java/com/niyonkuruelisa/umuriro/MainActivity.java
@@ -2,7 +2,6 @@ package com.niyonkuruelisa.umuriro;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,89 +11,54 @@ import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.niyonkuruelisa.umuriro.services.StopAlarmReceiver;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IGNORE_BATTERY_OPTIMIZATIONS = 2;
-    private Button powerCheckButton;
     public boolean serviceStarted =  false;
-    private SwitchCompat buttonSwitch;
+    private SwitchCompat powerCheckSwitchButton;
+    @SuppressLint({"SetTextI18n", "InlinedApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        powerCheckButton = findViewById(R.id.start_checking_power);
-        powerCheckButton.setEnabled(false);
-        buttonSwitch = findViewById(R.id.buttonSwitch);
-        buttonSwitch.setChecked(false);
-        buttonSwitch.setText("Tangira gucunga");
+        powerCheckSwitchButton = findViewById(R.id.buttonSwitch);
+        powerCheckSwitchButton.setChecked(false);
+        powerCheckSwitchButton.setEnabled(false);
+        powerCheckSwitchButton.setText("Tangira gucunga");
         requestIgnoreBatteryOptimizations();
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS}, REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
         } else {
-            powerCheckButton.setEnabled(true);
+            powerCheckSwitchButton.setEnabled(true);
         }
 
-        buttonSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    powerCheckButton.setText("Hagarika gucunga...");
-                    buttonSwitch.setText("Hagarika gucunga...");
-                    serviceStarted =  true;
-                    startChargingService();
-                }else{
-                    powerCheckButton.setText("Tangira gucunga");
-                    buttonSwitch.setText("Tangira gucunga");
-                    //we need to stop the service
-                    serviceStarted = false;
-                    Intent stopIntent = new Intent(buttonView.getContext(), StopAlarmReceiver.class);
-                    buttonView.getContext().sendBroadcast(stopIntent);
-                    Intent intent = new Intent(MainActivity.this, com.niyonkuruelisa.umuriro.services.ChargingService.class);
-                    stopService(intent);
-                }
+        powerCheckSwitchButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                powerCheckSwitchButton.setText("Hagarika gucunga...");
+                serviceStarted =  true;
+                startChargingService();
+            }else{
+                powerCheckSwitchButton.setText("Tangira gucunga");
+                //we need to stop the service
+                serviceStarted = false;
+                Intent stopIntent = new Intent(buttonView.getContext(), StopAlarmReceiver.class);
+                buttonView.getContext().sendBroadcast(stopIntent);
+                Intent intent = new Intent(MainActivity.this, com.niyonkuruelisa.umuriro.services.ChargingService.class);
+                stopService(intent);
             }
         });
-        powerCheckButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("checking","clicked");
-               if(!serviceStarted){
-                   Log.d("checking","going to start a service");
-                   powerCheckButton.setText("Hagarika gucunga...");
-                   buttonSwitch.setText("Hagarika gucunga...");
-                   serviceStarted =  true;
-                   startChargingService();
-               }else{
 
-                   Log.d("checking","going to stop a service");
-                   powerCheckButton.setText("Tangira gucunga");
-                   buttonSwitch.setText("Tangira gucunga");
-                    //we need to stop the service
-                   serviceStarted = false;
-                   Intent stopIntent = new Intent(v.getContext(), StopAlarmReceiver.class);
-                   v.getContext().sendBroadcast(stopIntent);
-                   Intent intent = new Intent(MainActivity.this, com.niyonkuruelisa.umuriro.services.ChargingService.class);
-                   stopService(intent);
-               }
-
-            }
-        });
     }
     private void requestIgnoreBatteryOptimizations() {
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
@@ -111,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_IGNORE_BATTERY_OPTIMIZATIONS) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                powerCheckButton.setEnabled(true);
+                powerCheckSwitchButton.setEnabled(true);
             } else {
                 // Permission denied, handle accordingly
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS}, REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
